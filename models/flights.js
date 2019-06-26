8a1bb13ed49536b806a9a22860ba8b6aaf3124f8
Wyrafinowany <zajac_dominik@onet.pl>
2019-06-26 14:13:24 +0200

    Add styles to navigation

--- models/flights.js
+++ models/flights.js
@@ -6,7 +6,20 @@ const newsSchema = new Schema({
         type: String,
         required: true
     },
+    departureDate: {
+        type: Date,
+        required: true
+    },
+    arrivelDate: {
+        type: Date,
+        default: +new Date() + 7 * 24 * 60 * 60 * 1000,
+        required: true
+    },
     places: {
+        type: Number,
+        required: true
+    },
+    ticketPrice: {
         type: Number,
         required: true
     },
